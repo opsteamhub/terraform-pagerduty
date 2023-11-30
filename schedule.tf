853382f6bfadd2373b430a7b66c87068348de767
Rafael Vinicius Cardoso Julio <rafael@macbook.local>
2023-11-30 12:07:41 -0300

    teste layer

--- schedule.tf
+++ schedule.tf
@@ -37,17 +37,17 @@ resource "pagerduty_schedule" "schedule" {
 }
 
 
-data "pagerduty_user" "users" {
-  for_each = zipmap(
-    distinct(flatten(values(var.schedule)[*]["layer"]["users"])),
-    distinct(flatten(values(var.schedule)[*]["layer"]["users"]))
-  )
-  email = each.value
-
-  depends_on = [
-    pagerduty_user.user
-  ]
-}
+#data "pagerduty_user" "users" {
+#  for_each = zipmap(
+#    distinct(flatten(values(var.schedule)[*]["layer"]["users"])),
+#    distinct(flatten(values(var.schedule)[*]["layer"]["users"]))
+#  )
+#  email = each.value
+#
+#  depends_on = [
+#    pagerduty_user.user
+#  ]
+#}
 
 #output "teste" {
 #  value = zipmap(

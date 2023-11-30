1bbb8c4a8bcf3faeb162a5b1e28a56fd11b77b0b
Rafael Vinicius Cardoso Julio <rafael@macbook.local>
2023-11-30 14:35:45 -0300

    teste data

--- schedule.tf
+++ schedule.tf
@@ -51,7 +51,7 @@ resource "pagerduty_schedule" "schedule" {
 
 data "pagerduty_user" "users" {
   for_each = {
-    for layer_name, layer in var.schedule : layer_name => length(values(layer.layer)) > 0 ? tolist(values(layer.layer)[0].users) : null
+    for layer_name, layer in var.schedule : layer_name => flatten([for user in values(layer.layer)[0].users : user])
   }
 
   email = each.value
